f34a31781a9cb92d77cd8ad8308009f5051ce565
Gregory <grzegorz.strachow@gmail.com>
2025-04-17 18:53:11 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 APP_NAME = 'Gregory Lab'   # Add
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+wvh$25*pnt*lh@rl2c^%f07(ou=j9+_$h!l$n_oi#=c1p02)#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
